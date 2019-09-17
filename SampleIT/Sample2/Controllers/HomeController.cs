e67714e573f15272e3ab5a2fc599cd09f3dc8023
sjinsjin <sjin@pkim.com>
2019-09-17 14:49:12 -0700

    first commit

---
+++ SampleIT/Sample2/Controllers/HomeController.cs
@@ -0,0 +1,86 @@
+﻿using Sample2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Helpers;
+
+
+namespace Sample2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Registration regg)
+        {
+            if (ModelState.IsValid)
+            {
+                WebMail.Send(to: "sjin@pkim.com", subject: "Request and Qustion", body: "<br/>Name  : " + regg.Name +
+                    "<br/>Phone Number: " + regg.Mobile + "<br/>Email  : " + regg.Email + "<br/>Message: " + regg.Message);
+            }
+
+            return RedirectToAction("Email");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Contact(Registration reg)
+        {
+            if (ModelState.IsValid)
+            {
+                WebMail.Send(to: "sjin@pkim.com", subject: "Website Contact information", body: "<br/>Name  : " + reg.Name +
+                    "<br/>Phone Number: " + reg.Mobile + "<br/>Email  : " + reg.Email + "<br/>Message: " + reg.Message);
+            }
+
+            return RedirectToAction("Email");
+        }
+
+        public ActionResult Service()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Aldelo()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Email()
+        {
+            return View();
+        }
+        public ActionResult Itsupport()
+        {
+            return View();
+        }
+        public ActionResult Security()
+        {
+            return View();
+        }
+        public ActionResult Software()
+        {
+            return View();
+        }
+    }
+}
---
+++ SampleIT/Sample2/Models/Registration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sample2.Models
+{
+    public class Registration
+    {
+        [Required(ErrorMessage="Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage="Email is required")]
+        [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$", ErrorMessage="Email format is not correct")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage="Mobile is required")]
+        public string Mobile { get; set; }
+
+        [Required(ErrorMessage="Message is required")]
+        public string Message { get; set; }
+    }
+}
